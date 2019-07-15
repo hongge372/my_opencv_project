daeb1b3c3098e3266ffd81cd59c25b28b388959a
hongge372 <hongge372@126.com>
2019-07-15 13:46:17 +0800

    first commit

---
+++ app/src/main/cpp/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by wanglizheng on 2019-07-13.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
---
+++ app/src/main/java/com/example/opencv3/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.opencv3;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.*;
+import org.opencv.samples.facedetect.FdActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+    };
+    private static int REQUEST_PERMISSION_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadSo();
+        requestPermission();
+        prepareOpencv();
+    }
+
+    private void prepareOpencv(){
+        Button button = (Button)findViewById(R.id.opecv_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.opecv_start:
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this, FdActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void requestPermission(){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+            }
+        }
+    }
+
+    private void loadSo(){
+        System.loadLibrary("detection_based_tracker");
+
+    }
+}
